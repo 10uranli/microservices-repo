49192b659e9bcac7da5a06e8b012b07ed6e4074d
10uranli <onur.anli@bil.omu.edu.tr>
2021-02-28 22:41:42 +0300

    load balancing

--- currency-conversion-service/currency-conversion-service/src/main/java/com/onuranli/restful/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/onuranli/restful/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000") /*spring.application.name=currency-exchange*/
+//@FeignClient(name = "currency-exchange", url = "localhost:8000") /*spring.application.name=currency-exchange*/
+@FeignClient(name = "currency-exchange") /*load balancing icin kapatıldı*/
+
 public interface CurrencyExchangeProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversion retrieveExhange(@PathVariable("from") String from, @PathVariable("to") String to);/*Currency exchange controllerdan aldım*/
