6528f2f83c78a38ab58c17323be363c03442e94e
10uranli <onur.anli@bil.omu.edu.tr>
2021-03-01 17:03:44 +0300

    api gateway links fix

---
+++ api-gateway/api-gateway/src/main/java/com/onuranli/restful/microservices/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,26 @@
+package com.onuranli.restful.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+				.route(p -> p.path("/get") /*pathlerin icinde /get ile gelen istege header ve param ekler*/
+						.filters(f -> f.addRequestHeader("MyHeader", "MyURI").addRequestParameter("MyParam", "MyValue"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange")) /*pathlerin icinde /currency-exchange ile gelen istege header ve param ekler*/
+				.route(p -> p.path("/currency-conversion/**").uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-feign/**").uri("lb://currency-conversion"))
+				.route(p -> p.path("/currency-conversion-new/**")
+						.filters(f -> f.rewritePath("/currency-conversion-new/(?<segment>.*)", "/currency-conversion-feign/${segment}"))
+						.uri("lb://currency-conversion"))
+				.build();
+	}
+}
