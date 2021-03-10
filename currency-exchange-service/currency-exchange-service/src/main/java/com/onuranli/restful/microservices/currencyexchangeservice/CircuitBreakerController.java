b4f5a8d7c75af14165ca1863fdc2846b321eb0ed
10uranli <onur.anli@bil.omu.edu.tr>
2021-03-10 23:38:38 +0300

    circuit breaker initialize

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,12 @@
+package com.onuranli.restful.microservices.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircuitBreakerController {
+	@GetMapping("/sample-api")
+	public String sampleApi(){
+		return "Default Api";
+	}
+}
