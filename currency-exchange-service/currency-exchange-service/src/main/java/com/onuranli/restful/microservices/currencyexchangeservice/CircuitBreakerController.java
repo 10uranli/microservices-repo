b1f8c474c4b6f3e2920cadccb6783fda5b94e165
10uranli <onur.anli@bil.omu.edu.tr>
2021-03-14 01:15:56 +0300

    circuit breaker retry and fallback method added

--- currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,12 +1,28 @@
 package com.onuranli.restful.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
+	Logger log = LoggerFactory.getLogger(CircuitBreakerController.class);
+	
 	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "defaultResponse")/*default name ile 3 kere yapar*/
 	public String sampleApi(){
-		return "Default Api";
+		log.info("Method sampleApi");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("htpp://localhost:8080/olmayanAdres", String.class);
+		return forEntity.getBody();
+	}
+	
+	public String defaultResponse(Exception ex){
+		return "Default Response!!";
+		
 	}
 }
