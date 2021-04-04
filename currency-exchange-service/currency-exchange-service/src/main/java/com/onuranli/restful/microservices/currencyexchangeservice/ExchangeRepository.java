4ae91d3354aa2410307a454db893e62d8f7113a0
10uranli <onur.anli@bil.omu.edu.tr>
2021-04-04 20:28:13 +0300

    git control

--- currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/ExchangeRepository.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/ExchangeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExchangeRepository extends JpaRepository<CurrencyExchange, Long>{
 
-	CurrencyExchange findByFromAndTo(String from,String to);
+	CurrencyExchange findByFromAndTo(String from,String to); 
 }
