7bb2fbe915041159859035ce967e30c8a035b639
onur.anli <onur.anli@bil.omu.edu.tr>
2023-12-20 00:39:55 +0300

    commit

--- currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/onuranli/restful/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
-
+//commit
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
